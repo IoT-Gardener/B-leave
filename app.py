9a52d4ba323da93508587a754a8e86951cd7ea3f
Alexander Billington <afbillington96@gmail.com>
2023-08-13 19:54:38 +0100

    Update now the poll has finished

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ async def countdown(container: st.delta_generator.DeltaGenerator):
     # Run infinitely
     while True:
         # Calculate the timedelta between a given date, and now.
-        time_diff = datetime.strptime("08/01/23 11:00:00", '%m/%d/%y %H:%M:%S') - datetime.now()
+        time_diff = datetime.strptime("10/09/23 09:00:00", '%m/%d/%y %H:%M:%S') - datetime.now()
         # Find the total number of seconds until the given date
         total_seconds = int(time_diff.seconds)
         # Calculate how many hours there are, and how many seconds are left
@@ -131,164 +131,164 @@ with st.container():
     #At some more text
     st.write("This will be a week long adventure to the wild lands of the UK (final location pending selection of a optimal date) where we will gathering a giant house/cabin/barn in the middle of nowhere and have a big ol' Holiday. There will be a fire, and the will most likely be a hot tub.")
     # Add even more text
-    st.write("The countdown is on until the events of B-Leave Summit 2023 are finalised. Confirm your attendence, and vote for a date that suits you!")
+    st.write("The countdown is on until B-Leave Summit 2023. Check the Whatsapp community for details!")
 
     # Create a container to store the timer
     countdown_container = st.empty()
 
-    survery_l, survey_r = st.columns((1,1))
-    with survery_l: 
-        # Add text input to enter name
-        name = st.text_input('Please enter your name.')
-        # Add checkbox to select if you are attending
-        attending = st.checkbox('I would like to attend B-Leave Summit 2023')
-        # Add radio button to answer the silly question
-        fight = st.radio("I would rather fight:", ('42 duck size horses', '1 horse size duck'))
+    # survery_l, survey_r = st.columns((1,1))
+    # with survery_l: 
+    #     # Add text input to enter name
+    #     name = st.text_input('Please enter your name.')
+    #     # Add checkbox to select if you are attending
+    #     attending = st.checkbox('I would like to attend B-Leave Summit 2023')
+    #     # Add radio button to answer the silly question
+    #     fight = st.radio("I would rather fight:", ('42 duck size horses', '1 horse size duck'))
 
-    with survey_r:
-        # Add multi choice to select dates
-        dates = st.multiselect('Which date(s) work for you? (You can select multiple options)', ['Week of 09/10', 'Week of 16/10'])
-        # Add radio button to select what you are
-        role = st.radio("I am:", ('a B-Leaver', 'a B-Wife', 'still suffering', 'other'))
+    # with survey_r:
+    #     # Add multi choice to select dates
+    #     dates = st.multiselect('Which date(s) work for you? (You can select multiple options)', ['Week of 09/10', 'Week of 16/10'])
+    #     # Add radio button to select what you are
+    #     role = st.radio("I am:", ('a B-Leaver', 'a B-Wife', 'still suffering', 'other'))
 
-    # Add a button
-    if st.button('Submit'):
-        # Assign week boolean based on multiselect responses
-        week_09 = True if 'Week of 09/10' in dates else False
-        week_16 = True if 'Week of 16/10' in dates else False
-        # Create dictionary of responses
-        submission = {
-            "Name": name,
-            "Attending": attending,
-            "09/10": week_09,
-            "16/10": week_16,
-            "Fight": fight,
-            "Role": role
-        }
-        # Attempt to upload response to DB
-        try:
-            col.insert_one(submission)
-            st.write("Submission successful!")
-        except:
-            st.write("Submission failed!")
-    # Add a line divider
-    st.write("---")
+    # # Add a button
+    # if st.button('Submit'):
+    #     # Assign week boolean based on multiselect responses
+    #     week_09 = True if 'Week of 09/10' in dates else False
+    #     week_16 = True if 'Week of 16/10' in dates else False
+    #     # Create dictionary of responses
+    #     submission = {
+    #         "Name": name,
+    #         "Attending": attending,
+    #         "09/10": week_09,
+    #         "16/10": week_16,
+    #         "Fight": fight,
+    #         "Role": role
+    #     }
+    #     # Attempt to upload response to DB
+    #     try:
+    #         col.insert_one(submission)
+    #         st.write("Submission successful!")
+    #     except:
+    #         st.write("Submission failed!")
+    # # Add a line divider
+#     # st.write("---")
 
 
-# Insight section
-with st.container():
-    # Add a title
-    st.header("B-Leaver Insights")
-    # Add some text
-    st.write("What would life be without some data?!")
+# # Insight section
+# with st.container():
+#     # Add a title
+#     st.header("B-Leaver Insights")
+#     # Add some text
+#     st.write("What would life be without some data?!")
 
-    # Read all the data from the database.
-    data = col.find()
-    # Convert data to a pandas dataframe
-    df = pd.DataFrame(list(data))
+#     # Read all the data from the database.
+#     data = col.find()
+#     # Convert data to a pandas dataframe
+#     df = pd.DataFrame(list(data))
 
-    # Get a list of the unique roles
-    roles = df['Role'].unique()
-    # Create list to store count for each role
-    role_counts = []
-    # Populate the list with the number of roles
-    for role in roles:
-        role_counts.append(df.Role.value_counts()[role])
-    # Create a list of dictionaries with the number of horse/duck fighters for each date
-    date_dicts = [
-        {
-            "Date": "09/10",
-            "type": "Duck fighter",
-            "Votes": len(df.loc[(df['09/10'] == True) & (df['Fight'] == "1 horse size duck") & (df['Attending'] == True)])
-        },
-        {
-            "Date": "09/10",
-            "type": "Horse fighter",
-            "Votes": len(df.loc[(df['09/10'] == True) & (df['Fight'] == "42 duck size horses") & (df['Attending'] == True)])
-        },
-        {
-            "Date": "16/10",
-            "type": "Duck fighter",
-            "Votes": len(df.loc[(df['16/10'] == True) & (df['Fight'] == "1 horse size duck") & (df['Attending'] == True)])
-        },
-        {
-            "Date": "16/10",
-            "type":  "Horse fighter",
-            "Votes": len(df.loc[(df['16/10'] == True) & (df['Fight'] == "42 duck size horses") & (df['Attending'] == True)])
-        }
-    ]
-    # Get only the names of people attending
-    names_going = df.loc[df['Attending'] == True, 'Name']
-    # Get the number of horse fighters
-    no_horse_fighters = len(df.loc[df['Fight'] == "1 horse size duck"])
-    # Get the number of duck fighters
-    no_duck_fighters = len(df.loc[df['Fight'] == "42 duck size horses"])
+#     # Get a list of the unique roles
+#     roles = df['Role'].unique()
+#     # Create list to store count for each role
+#     role_counts = []
+#     # Populate the list with the number of roles
+#     for role in roles:
+#         role_counts.append(df.Role.value_counts()[role])
+#     # Create a list of dictionaries with the number of horse/duck fighters for each date
+#     date_dicts = [
+#         {
+#             "Date": "09/10",
+#             "type": "Duck fighter",
+#             "Votes": len(df.loc[(df['09/10'] == True) & (df['Fight'] == "1 horse size duck") & (df['Attending'] == True)])
+#         },
+#         {
+#             "Date": "09/10",
+#             "type": "Horse fighter",
+#             "Votes": len(df.loc[(df['09/10'] == True) & (df['Fight'] == "42 duck size horses") & (df['Attending'] == True)])
+#         },
+#         {
+#             "Date": "16/10",
+#             "type": "Duck fighter",
+#             "Votes": len(df.loc[(df['16/10'] == True) & (df['Fight'] == "1 horse size duck") & (df['Attending'] == True)])
+#         },
+#         {
+#             "Date": "16/10",
+#             "type":  "Horse fighter",
+#             "Votes": len(df.loc[(df['16/10'] == True) & (df['Fight'] == "42 duck size horses") & (df['Attending'] == True)])
+#         }
+#     ]
+#     # Get only the names of people attending
+#     names_going = df.loc[df['Attending'] == True, 'Name']
+#     # Get the number of horse fighters
+#     no_horse_fighters = len(df.loc[df['Fight'] == "1 horse size duck"])
+#     # Get the number of duck fighters
+#     no_duck_fighters = len(df.loc[df['Fight'] == "42 duck size horses"])
 
-    # Create 3 columns for metrics
-    metrics_l, metrics_m, metrics_r = st.columns((1,1,1))
-    with metrics_l:
-    # Display a metric with the number of attendees
-        st.metric(
-            label="Summit Attendees",
-            value=f"{len(names_going)}/{len(df)}",
-            help="The total number of people who have said they are attending."
-        )
-    with metrics_m:
-        # Add metric with number of horse fighters
-        st.metric(
-            label="Horse fighters",
-            value=no_horse_fighters,
-            delta=no_horse_fighters-no_duck_fighters,
-            help="The number of people who would rather fight 42 duck size horses"
-        )
-    with metrics_r:
-        # Add metric with number of duck fighters
-        st.metric(
-            label="Duck fighters",
-            value=no_duck_fighters,
-            delta=no_duck_fighters-no_horse_fighters,
-            help="The number of people who would rather fight 1 horse size duck"
-        )
+#     # Create 3 columns for metrics
+#     metrics_l, metrics_m, metrics_r = st.columns((1,1,1))
+#     with metrics_l:
+#     # Display a metric with the number of attendees
+#         st.metric(
+#             label="Summit Attendees",
+#             value=f"{len(names_going)}/{len(df)}",
+#             help="The total number of people who have said they are attending."
+#         )
+#     with metrics_m:
+#         # Add metric with number of horse fighters
+#         st.metric(
+#             label="Horse fighters",
+#             value=no_horse_fighters,
+#             delta=no_horse_fighters-no_duck_fighters,
+#             help="The number of people who would rather fight 42 duck size horses"
+#         )
+#     with metrics_r:
+#         # Add metric with number of duck fighters
+#         st.metric(
+#             label="Duck fighters",
+#             value=no_duck_fighters,
+#             delta=no_duck_fighters-no_horse_fighters,
+#             help="The number of people who would rather fight 1 horse size duck"
+#         )
 
-    # Create two columns for insights
-    insights_l, insights_r = st.columns((1,1))
+#     # Create two columns for insights
+#     insights_l, insights_r = st.columns((1,1))
 
-    # Populate the left column
-    with insights_l:
-        st.write("Who is going?")
-        # Create word cloud use given shape and colouring
-        wordcloud = WordCloud(repeat=True, mask=np.array(mask_img)).generate(" ".join(names_going   ))
-        wordcloud.recolor(color_func=ImageColorGenerator(np.array(mask_img)))
-        # Display the generated word cloud
-        st.image(wordcloud.to_array(), use_column_width=True)
+#     # Populate the left column
+#     with insights_l:
+#         st.write("Who is going?")
+#         # Create word cloud use given shape and colouring
+#         wordcloud = WordCloud(repeat=True, mask=np.array(mask_img)).generate(" ".join(names_going   ))
+#         wordcloud.recolor(color_func=ImageColorGenerator(np.array(mask_img)))
+#         # Display the generated word cloud
+#         st.image(wordcloud.to_array(), use_column_width=True)
 
-    # Populate the right column
-    with insights_r:
-        st.write("Who makes up the B-Leavers")
-        source = pd.DataFrame({"category": roles, "values": role_counts})
-        # Create pie chart
-        base = alt.Chart(source).encode(
-            theta=alt.Theta("values:Q", stack=True),
-            radius=alt.Radius("values", scale=alt.Scale(type="sqrt", zero=True, rangeMin=20)),
-            color=alt.Color("category:N", scale=alt.Scale(scheme="darkred"))
-        )
-        # Add in inner cirlce
-        c1 = base.mark_arc(innerRadius=20, stroke="#fff")
-        # Display the chart
-        st.altair_chart(c1, use_container_width=True)
+#     # Populate the right column
+#     with insights_r:
+#         st.write("Who makes up the B-Leavers")
+#         source = pd.DataFrame({"category": roles, "values": role_counts})
+#         # Create pie chart
+#         base = alt.Chart(source).encode(
+#             theta=alt.Theta("values:Q", stack=True),
+#             radius=alt.Radius("values", scale=alt.Scale(type="sqrt", zero=True, rangeMin=20)),
+#             color=alt.Color("category:N", scale=alt.Scale(scheme="darkred"))
+#         )
+#         # Add in inner cirlce
+#         c1 = base.mark_arc(innerRadius=20, stroke="#fff")
+#         # Display the chart
+#         st.altair_chart(c1, use_container_width=True)
 
-    st.write("What is the best date?")
-    source = pd.DataFrame(date_dicts)
-    # Create a bar chart
-    base2 = alt.Chart(source).mark_bar().encode(
-        x='Votes',
-        y='Date',
-        color=alt.Color('type', scale=alt.Scale(scheme="darkred"))
-    )
-    # Display the chart
-    st.altair_chart(base2, use_container_width=True)
+#     st.write("What is the best date?")
+#     source = pd.DataFrame(date_dicts)
+#     # Create a bar chart
+#     base2 = alt.Chart(source).mark_bar().encode(
+#         x='Votes',
+#         y='Date',
+#         color=alt.Color('type', scale=alt.Scale(scheme="darkred"))
+#     )
+#     # Display the chart
+#     st.altair_chart(base2, use_container_width=True)
 
-    st.write("---")
+#     st.write("---")
 
 
 # Footer section
